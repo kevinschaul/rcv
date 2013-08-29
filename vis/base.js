444e059ae35f4ba64893e0dc8804bece0055c32d
Kevin Schaul <kevin.schaul@gmail.com>
2013-08-29 11:33:42 -0500

    take two

--- vis/base.js
+++ vis/base.js
@@ -4,60 +4,66 @@ var height = 500 - margin.top - margin.bottom;
 
 var svg = d3.select('.target').append('svg')
   .attr('width', 960)
-  .attr('height', 500)
+  .attr('height', 800)
     .append('g')
   .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
 var data = [
-  // Each index at this level is a round
-  [
-    // Each index is a candidate
-    // [[number of votes, candidate index votes came from], ...]
-    [[22, 0]],
-    [[3, 1]],
-    [[20, 2]]
-  ],
-  [
-    [[22, 0], [2, 1]],
-    [[0, 1]],
-    [[20, 2], [1, 1]]
-  ]
+  [30, [0, 0, 0]],
+  [5, [1, 0, 0]],
+  [2, [1, 2, 2]],
+  [2, [1, 3, 2]],
+  [20, [2, 2, 2]],
+  [15, [3, 3, 0]],
+  [10, [3, 3, 2]]
 ];
-
-var numberOfCandidates = 3;
-var betweenCandidates = 50;
-
-var line = d3.svg.line()
-  .x(function(d) { return d[0][1]; })
-  .y(function(d) { return d[0][1]; })
 
 console.log(data);
 
-_.each(data, function(round, roundIndex) {
+var numberOfRounds = 3;
+var numberOfCandidates = 4;
+var hPadding = 0; // TODO
+var vPadding = 200;
+var candidateWidth = width / (numberOfCandidates);
 
+var cumulativeVotes = [0, 0, 0, 0];
+
+var x = d3.scale.linear()
+  .domain([0, 100])
+  .range([0, width / numberOfCandidates])
+
+var line = d3.svg.line()
+  .x(function(d) {
+    return d.candidate * candidateWidth;
+  })
+  .y(function(d, i) {
+    return i * vPadding;
+  })
+
+_.each(_.range(0, numberOfRounds), function(roundIndex) {
   _.each(_.range(0, numberOfCandidates), function(i) {
     svg.append('rect')
       .attr('class', 'guide')
-      .attr('x', function(d) { return (i * (width / numberOfCandidates)) + (betweenCandidates / 2); })
-      .attr('y', function(d) { return roundIndex * 200; })
-      .attr('width', (width / numberOfCandidates) - betweenCandidates)
+      .attr('x', function(d) { return (i * (width / numberOfCandidates)) + (hPadding / 2); })
+      .attr('y', function(d) { return roundIndex * vPadding; })
+      .attr('width', (width / numberOfCandidates) - hPadding)
       .attr('height', 30);
   });
-
-  svg.selectAll('path')
-      .data(data[roundIndex])
-    .enter().append('path')
-      .attr('d', function(d) {
-        console.log(d);
-        return line(d);
-      })
-
 });
 
-/*
 svg.selectAll('path')
     .data(data)
   .enter().append('path')
-    .attr('d', function(d) { console.log(d); return line(d); });
-   */
+    .attr('class', 'vote-line')
+    .style('stroke-width', function(d) { return d[0]; })
+    .attr('d', function(d, i) {
+      var lineData = [];
+      for (var i = 0; i < d[1].length; i++) {
+        lineData.push({
+          width: d[0],
+          candidate: d[1][i]
+        });
+      }
+      return line(lineData);
+    })
 
